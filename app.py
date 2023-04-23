b1d959d6e8cfa1dc73d547586fdb6cb3f180fef9
huanghaian <huanghaian@sensetime.com>
2023-04-23 12:17:16 +0800

    add others model

--- app.py
+++ app.py
@@ -9,38 +9,35 @@ os.system('python -m mim install -e .')
 from argparse import ArgumentParser
 
 import gradio as gr
-from mmdet.apis import inference_detector, init_detector
+from mmdet.apis import DetInferencer, inference_detector
 
-from mmyolo.registry import VISUALIZERS
 from mmyolo.utils import switch_to_deploy
 
+model_dict = {
+    'RTMDet-L': 'rtmdet_l_syncbn_fast_8xb32-300e_coco',
+    'YOLOv5-L': 'yolov5_l-v61_syncbn_fast_8xb16-300e_coco',
+    'YOLOv6-L': 'yolov6_l_syncbn_fast_8xb32-300e_coco',
+    'YOLOv7-L': 'yolov7_l_syncbn_fast_8x16b-300e_coco',
+    'YOLOv8-L': 'yolov8_l_mask-refine_syncbn_fast_8xb16-500e_coco',
+    'YOLOX-L': 'yolox_l_fast_8xb8-300e_coco',
+    'PPYOLOE-L': 'ppyoloe_plus_L_fast_8xb8-80e_coco',
+}
 
-def inference(input):
+
+def inference(input, model_str):
     parser = ArgumentParser()
-    parser.add_argument(
-        '--config',
-        default='configs/rtmdet/rtmdet_l_syncbn_fast_8xb32-300e_coco.py',
-        help='Config file')
-    parser.add_argument(
-        '--checkpoint',
-        default=
-        'https://download.openmmlab.com/mmyolo/v0/rtmdet/rtmdet_l_syncbn_fast_8xb32-300e_coco/rtmdet_l_syncbn_fast_8xb32-300e_coco_20230102_135928-ee3abdc4.pth',
-        help='Checkpoint file')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
         '--score-thr', type=float, default=0.3, help='Bbox score threshold')
     args = parser.parse_args()
 
-    model = init_detector(args.config, args.checkpoint, device=args.device)
-    switch_to_deploy(model)
+    det_inferencer = DetInferencer(model_dict[model_str], scope='mmyolo')
+    switch_to_deploy(det_inferencer.model)
 
-    visualizer = VISUALIZERS.build(model.cfg.visualizer)
-    visualizer.dataset_meta = model.dataset_meta
+    result = inference_detector(det_inferencer.model, input)
 
-    result = inference_detector(model, input)
-
-    visualizer.add_datasample(
+    det_inferencer.visualizer.add_datasample(
         'image',
         input,
         data_sample=result,
@@ -49,14 +46,18 @@ def inference(input):
         wait_time=0,
         pred_score_thr=args.score_thr)
 
-    return visualizer.get_image()
+    return det_inferencer.visualizer.get_image()
 
+
+image = gr.Image(type='numpy', label='input')
+input_model = gr.inputs.Dropdown(
+    choices=list(model_dict.keys()), label='select model', default='RTMDet-L')
 
 gr.Interface(
     fn=inference,
     title='MMYOLO Demo',
     description=
     'Note: The first time running requires downloading the weights, please wait a moment.',  # noqa
-    inputs=gr.Image(type='numpy'),
+    inputs=[image, input_model],
     outputs=gr.Image(type='pil'),
-    examples=['demo/dog.jpg']).launch()
+    examples=[['demo/dog.jpg', 'RTMDet-L']]).launch()
